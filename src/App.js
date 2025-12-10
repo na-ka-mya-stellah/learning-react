467c0470d203e10a44b63b8f31594fd6a1360cbb
na-ka-mya-stellah <nstellah285@gmail.com>
2025-12-10 23:48:51 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Read from "./components/Read"; // import the file, React will get the function
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Read />   {/* Use it like a JSX component */}
     </div>
   );
 }
---
+++ src/components/Read.jsx
@@ -0,0 +1,4 @@
+function Read(){
+    return <h1>you read well</h1>
+}
+export default Read;
